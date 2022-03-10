dbc95687775464944bef341408a670b98fb29c75
Yang Cheng <annacheng09@hotmail.com>
2022-03-10 12:59:02 -0800

    adding classes to elements for styling

--- assets/js/search.js
+++ assets/js/search.js
@@ -17,32 +17,35 @@ function getImage(event) {
         })
         .then(function (data) {
         console.log(data);
-        let apiData = data.data
+        let apiData = data.data;
         for (let i=0; i< apiData.length; i++) {
-            let animeTitle = document.createElement('h2')
-            animeTitle.classList.add("anime-title", "center")
+            let animeTitle = document.createElement('h2');
+            animeTitle.classList.add("anime-title", "center");
             if (!apiData[i].title_english) {
                 animeTitle.textContent = apiData[i].title
             } else {
                 animeTitle.textContent = apiData[i].title_english
             }
-            let animeImage = document.createElement('img')
-            animeImage.setAttribute("src", apiData[i].images.jpg.image_url)
-            animeImage.classList.add("anime-image", "center")
-            let animeRating = document.createElement('h6')
-            animeRating.textContent = "Rating: " + apiData[i].score
-            let animeGenres = document.createElement('h6')
-            let totalGenres = []
+            let animeImage = document.createElement('img');
+            animeImage.setAttribute("src", apiData[i].images.jpg.image_url);
+            animeImage.classList.add("anime-image", "center");
+            let animeRating = document.createElement('h6');
+            animeRating.textContent = "Rating: " + apiData[i].score;
+            animeRating.classList.add("anime-info");
+            let animeGenres = document.createElement('h6');
+            let totalGenres = [];
             for (let j=0; j< apiData[i].genres.length; j++) {
-                totalGenres.push(apiData[i].genres[j].name)
+                totalGenres.push(apiData[i].genres[j].name);
             }
-            animeGenres.textContent = "Genres: " + totalGenres.join(" ")
-            let animeStatus = document.createElement('h6')
-            animeStatus.textContent = "Status: " + apiData[i].status
-            let animeSyn = document.createElement('p')
-            animeSyn.textContent = apiData[i].synopsis
-            animeSyn.classList.add("anime-synopsis", "center")
-            contentContainer.append(animeTitle, animeImage, animeRating, animeGenres, animeStatus, animeSyn)
+            animeGenres.textContent = "Genres: " + totalGenres.join(" ");
+            animeGenres.classList.add("anime-info")
+            let animeStatus = document.createElement('h6');
+            animeStatus.textContent = "Status: " + apiData[i].status;
+            animeStatus.classList.add("anime-info")
+            let animeSyn = document.createElement('p');
+            animeSyn.textContent = apiData[i].synopsis;
+            animeSyn.classList.add("anime-synopsis", "center");
+            contentContainer.append(animeTitle, animeImage, animeRating, animeGenres, animeStatus, animeSyn);
         }
         });
     
