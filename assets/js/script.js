0b154608c7a9320169a61df4a603fa1b4b4e258d
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-10 15:28:23 -0800

    function for the button to redirect to second html

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+let showResults = document.querySelector('.results');
+let searchButton = document.querySelector('.search-button');
+
+function handleSearchFromSubmit (event) {
+    event.preventDefault();
+
+    let searchInputVal = document.querySelector('#search-input').value;
+
+    if (!searchInputVal) {
+        console.error(' You need to put the name of the anime');
+        return;
+    }
+
+    let queryString = './search.html?q=' + searchInputVal ;
+
+    location.assign(queryString);
+}   
+
+searchButton.addEventListener('click', handleSearchFromSubmit);
