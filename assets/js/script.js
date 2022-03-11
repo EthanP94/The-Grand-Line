eec32158d96ee953db8e99d95ce8fd8f6962dcdf
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-11 15:39:52 -0800

    more css and styling for homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,6 @@ let showResults = document.querySelector('.results');
 let searchButton = document.querySelector('.search-button');
 let top10Button = document.querySelector('.top10-btn')
 
-let backgrounds = [
-'https://images4.alphacoders.com/102/thumb-1920-1028306.png',
-'https://i.pinimg.com/originals/16/65/a7/1665a7ae96ba9e767182d472c8f4f52e.jpg',
-'https://i.pinimg.com/originals/e1/0a/66/e10a6632d9ec2efa130120cc55d7e49e.png'
-];
-
-let randomEndpoint = backgrounds[Math.floor(Math.random()*backgrounds.length)]
-
-document.body.style.background = "url("+randomEndpoint+")"
-
 function handleSearchFromSubmit (event) {
     event.preventDefault();
 
@@ -27,10 +17,6 @@ function handleSearchFromSubmit (event) {
     location.assign(queryString);
 }   
 
-
-
-// homepage search button above
-
 let backgrounds = ['./assets/images/backgrounds/demon-slayer.png',
 './assets/images/backgrounds/berserk-2.png',
 './assets/images/backgrounds/one-piece.png',
@@ -41,11 +27,6 @@ let randomEndpoint = backgrounds[Math.floor(Math.random()*backgrounds.length)]
 document.body.style.background = "url("+randomEndpoint+")"
 document.body.style.backgroundSize = "cover";
 
-
-
-
-
-    
 
 
 function redirectTop() {
