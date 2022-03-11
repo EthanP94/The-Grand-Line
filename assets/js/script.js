7b9c67924d63fe44305efe1c445ee0662e779358
EthanP94 <99214691+EthanP94@users.noreply.github.com>
2022-03-11 15:21:29 -0800

    worked on css for homepage. Styling of the header and search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,10 @@ let backgrounds = ['./assets/images/backgrounds/demon-slayer.png',
 let randomEndpoint = backgrounds[Math.floor(Math.random()*backgrounds.length)]
 
 document.body.style.background = "url("+randomEndpoint+")"
+document.body.style.backgroundSize = "cover";
+
+
+
 
 
     
