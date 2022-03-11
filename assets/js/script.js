0a3033f81c284b13922267a3b3cb30aa05a93f45
LinnetteCapul <linnette.capul@gmail.com>
2022-03-11 15:59:19 -0800

    fixed buy anime link in nav bar to open new tab in both html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,7 @@ let showResults = document.querySelector('.results');
 let searchButton = document.querySelector('.search-button');
 let top10Button = document.querySelector('.top10-btn')
 
-let backgrounds = [
-'https://images4.alphacoders.com/102/thumb-1920-1028306.png',
-'https://i.pinimg.com/originals/16/65/a7/1665a7ae96ba9e767182d472c8f4f52e.jpg',
-'https://i.pinimg.com/originals/e1/0a/66/e10a6632d9ec2efa130120cc55d7e49e.png'
-];
 
-let randomEndpoint = backgrounds[Math.floor(Math.random()*backgrounds.length)]
-
-document.body.style.background = "url("+randomEndpoint+")"
 
 function handleSearchFromSubmit (event) {
     event.preventDefault();
