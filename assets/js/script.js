c05d8b7fc384cf1b70970cc448c49fa3529554a3
LinnetteCapul <linnette.capul@gmail.com>
2022-03-14 10:13:37 -0700

    saving point to do merge main branch for up to date code

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,6 +27,34 @@ let randomEndpoint = backgrounds[Math.floor(Math.random()*backgrounds.length)]
 document.body.style.background = "url("+randomEndpoint+")"
 document.body.style.backgroundSize = "cover";
 
+
+// Genres list redirects homepage to specific list of genre the user clicked
+let genresListEl = document.querySelector('.genres-homepage')
+
+genresListEl.addEventListener("click", function(event) {
+    contentContainer.textContent = " ";
+    console.log(event.target)
+    if (event.target.matches("a")) {
+        let genre = event.target.getAttribute("data-genre")
+        console.log(genre)
+        requestUrl = "https://api.jikan.moe/v4/anime?genres=" + genre
+        console.log(requestUrl);
+        getAnimeInfo()
+    }
+
+})
+
+// function redirectGenres() {
+//    let requestUrl = "https://api.jikan.moe/v4/anime?q=";
+//    if (requestUrl.target.matches("a")){
+//        let genre = requestUrl.target.getAttribute("data-genre")
+//        requestUrl = "https://api.jikan.moe/v4/anime?genres=" + genre
+//        console.log(requestUrl);
+//        redirectGenres()
+//    }
+
+// }
+
 function redirectTop() {
     let queryString = './search.html?q=top-10';
     location.assign(queryString)
